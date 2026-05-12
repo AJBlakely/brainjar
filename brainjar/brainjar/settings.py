66cda2f5d0300bbfcca5b575d7fa4e0ffb897e75
jhakarib <jhakari1@yahoo.com>
2026-05-12 16:29:31 -0400

    deploy configs

--- brainjar/brainjar/settings.py
+++ brainjar/brainjar/settings.py
@@ -18,11 +18,6 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
-    )
-}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
